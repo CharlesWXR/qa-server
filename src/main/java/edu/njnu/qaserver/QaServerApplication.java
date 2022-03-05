eb2fd8a782f340fa2ef1ec117ab367f20012f059
CharlesWXR <19190211@stu.njnu.edu.cn>
2022-03-05 20:36:07 +0800

    MyBatis-Plus added

--- src/main/java/edu/njnu/qaserver/QaServerApplication.java
+++ src/main/java/edu/njnu/qaserver/QaServerApplication.java
@@ -1,9 +1,11 @@
 package edu.njnu.qaserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("edu.njnu.qaserver.mapper")
 public class QaServerApplication {
 
 	public static void main(String[] args) {
