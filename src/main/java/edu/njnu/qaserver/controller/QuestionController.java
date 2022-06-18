cb2ca45d0ea476d4b0f5040fc215c7b5278f26b7
BossWangST <bosswang1226@gmail.com>
2022-06-18 10:17:13 +0800

    Now you can upload answer to a question(With img).

--- src/main/java/edu/njnu/qaserver/controller/QuestionController.java
+++ src/main/java/edu/njnu/qaserver/controller/QuestionController.java
@@ -19,8 +19,8 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/question")
 public class QuestionController {
-	@Autowired
-	private QuestionService questionService;
+    @Autowired
+    private QuestionService questionService;
 
     @Autowired
     private AnswerService answerService;
@@ -36,11 +36,11 @@ public class QuestionController {
     }
 
 
-	@ResponseResult
-	@RequestMapping(value = "/{userID}", method = RequestMethod.GET)
-	public QuestionBriefsVO getQuestionByUser(@PathVariable int userID) {
-		return questionService.getQuestionByUser(userID);
-	}
+    @ResponseResult
+    @RequestMapping(value = "/{userID}", method = RequestMethod.GET)
+    public QuestionBriefsVO getQuestionByUser(@PathVariable int userID) {
+        return questionService.getQuestionByUser(userID);
+    }
 
     @ResponseResult
     @RequestMapping(value = "/id/{questionID}", method = RequestMethod.GET)
@@ -49,64 +49,78 @@ public class QuestionController {
     }
 
 
-	@ResponseResult
-	@RequestMapping(value = "/count", method = RequestMethod.GET)
-	public List<SubjectQuestionStat> getQuestionStats() {
-		return questionService.getQuestionStats();
-	}
-
-	@ResponseResult
-	@RequestMapping(value = "", method = RequestMethod.PUT)
-	public Map<String, Object> putNewQuestion(HttpServletRequest request) throws Exception {
-		String title = request.getParameter("title");
-		String mainContent = request.getParameter("main_content");
-		String subject = request.getParameter("subject");
-		MultipartFile file = ((MultipartHttpServletRequest) request).getFile("img");
-		int credit = Integer.parseInt(request.getParameter("credit"));
-		int userID = Integer.parseInt(request.getParameter("user_id"));
-		String tagRaw = request.getParameter("tags");
-		List<Integer> tags = null;
-		if (tagRaw != null && tagRaw.length() > 0) {
-			String[] tagStrings = tagRaw.split(",");
-			tags = Arrays.stream(tagStrings)
-							.map(s -> Integer.parseInt(s))
-							.collect(Collectors.toList());
-		}
-
-
-		Map<String, Object> res = new HashMap<String, Object>();
-		res.put("url",
-				questionService.putNewQuestion(title, mainContent, subject,
-						credit, userID, tags, file));
-		return res;
-	}
-
-	@ResponseResult
-	@RequestMapping(value = "/s", method = RequestMethod.GET)
-	public QuestionBriefsVO searchQuestion(@RequestParam String content,
-	                                            @RequestParam String subjectName,
-	                                            @RequestParam List<Integer> tags,
-	                                            @RequestParam long page) {
-		if (tags != null && tags.size() > 0)
-			return questionService.searchQuestion(content, subjectName, tags, page);
-		else
-			return questionService.searchQuestion(content, subjectName, page);
-	}
-
-	@ResponseResult
-	@RequestMapping(value = "/c", method = RequestMethod.GET)
-	public List<String> completeQuestion(@RequestParam String content,
-	                                     @RequestParam String subjectName,
-	                                     @RequestParam List<Integer> tags) {
-		if (tags != null && tags.size() > 0)
-			return questionService.completeQuestion(content, subjectName, tags);
-		else
-			return questionService.completeQuestion(content, subjectName);
-	}
+    @ResponseResult
+    @RequestMapping(value = "/count", method = RequestMethod.GET)
+    public List<SubjectQuestionStat> getQuestionStats() {
+        return questionService.getQuestionStats();
+    }
 
     @ResponseResult
-    @RequestMapping(value="/answer/{questionID}",method = RequestMethod.GET)
-    public List<AnswerVO> getAnswerByQuestionID(@PathVariable("questionID") int questionID){
+    @RequestMapping(value = "", method = RequestMethod.PUT)
+    public Map<String, Object> putNewQuestion(HttpServletRequest request) throws Exception {
+        String title = request.getParameter("title");
+        String mainContent = request.getParameter("main_content");
+        String subject = request.getParameter("subject");
+        MultipartFile file = ((MultipartHttpServletRequest) request).getFile("img");
+        int credit = Integer.parseInt(request.getParameter("credit"));
+        int userID = Integer.parseInt(request.getParameter("user_id"));
+        String tagRaw = request.getParameter("tags");
+        List<Integer> tags = null;
+        if (tagRaw != null && tagRaw.length() > 0) {
+            String[] tagStrings = tagRaw.split(",");
+            tags = Arrays.stream(tagStrings)
+                    .map(s -> Integer.parseInt(s))
+                    .collect(Collectors.toList());
+        }
+
+
+        Map<String, Object> res = new HashMap<String, Object>();
+        res.put("url",
+                questionService.putNewQuestion(title, mainContent, subject,
+                        credit, userID, tags, file));
+        return res;
+    }
+
+    @ResponseResult
+    @RequestMapping(value = "/s", method = RequestMethod.GET)
+    public QuestionBriefsVO searchQuestion(@RequestParam String content,
+                                           @RequestParam String subjectName,
+                                           @RequestParam List<Integer> tags,
+                                           @RequestParam long page) {
+        if (tags != null && tags.size() > 0)
+            return questionService.searchQuestion(content, subjectName, tags, page);
+        else
+            return questionService.searchQuestion(content, subjectName, page);
+    }
+
+    @ResponseResult
+    @RequestMapping(value = "/c", method = RequestMethod.GET)
+    public List<String> completeQuestion(@RequestParam String content,
+                                         @RequestParam String subjectName,
+                                         @RequestParam List<Integer> tags) {
+        if (tags != null && tags.size() > 0)
+            return questionService.completeQuestion(content, subjectName, tags);
+        else
+            return questionService.completeQuestion(content, subjectName);
+    }
+
+    @ResponseResult
+    @RequestMapping(value = "/answer/{questionID}", method = RequestMethod.GET)
+    public List<AnswerVO> getAnswerByQuestionID(@PathVariable("questionID") int questionID) {
         return answerService.getAnswerByQuestionID(questionID);
+    }
+
+    @ResponseResult
+    @RequestMapping(value = "/newAnswer/{questionID}", method = RequestMethod.PUT)
+    public Map<String, Object> putNewAnswer(HttpServletRequest request,
+                                            @PathVariable("questionID") int questionID) throws Exception {
+        String mainContent = request.getParameter("mainContent");
+        MultipartFile file = ((MultipartHttpServletRequest) request).getFile("img");
+        int userID = Integer.parseInt(request.getParameter("user_id"));
+
+        Map<String, Object> res = new HashMap<>();
+        res.put("url", answerService.putNewAnswer(questionID, mainContent, userID, file));
+
+        return res;
     }
 }
